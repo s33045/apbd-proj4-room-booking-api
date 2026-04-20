ef12d8e29edb3d1d7d275c4820c36254c04b84bf
s33045 <s33045@pjwstk.edu.pl>
2026-04-20 21:20:34 +0200

    feat(services): add room service

---
+++ RoomBookingApi/RoomBookingApi/Services/IRoomService.cs
@@ -0,0 +1,14 @@
+﻿using RoomBookingApi.Models;
+
+namespace RoomBookingApi.Services;
+
+public interface IRoomService
+{
+    IEnumerable<Room> GetAll();
+    Room? GetById(int id);
+    IEnumerable<Room> GetByBuildingCode(string buildingCode);
+    IEnumerable<Room> GetFiltered(int? minCapacity, bool? hasProjector, bool? activeOnly);
+    void Add(Room room);
+    bool Update(Room room);
+    bool Delete(int id);
+}
---
+++ RoomBookingApi/RoomBookingApi/Services/RoomService.cs
@@ -0,0 +1,42 @@
+﻿using RoomBookingApi.Models;
+using RoomBookingApi.Repositories;
+
+namespace RoomBookingApi.Services;
+
+public class RoomService(IRoomRepository roomRepository) : IRoomService
+{
+    public IEnumerable<Room> GetAll()
+    {
+        return roomRepository.GetAll();
+    }
+
+    public Room? GetById(int id)
+    {
+        return roomRepository.GetById(id);
+    }
+
+    public IEnumerable<Room> GetByBuildingCode(string buildingCode)
+    {
+        return roomRepository.GetByBuildingCode(buildingCode);
+    }
+
+    public IEnumerable<Room> GetFiltered(int? minCapacity, bool? hasProjector, bool? activeOnly)
+    {
+        return roomRepository.GetFiltered(minCapacity, hasProjector, activeOnly);
+    }
+
+    public void Add(Room room)
+    {
+        roomRepository.Add(room);
+    }
+
+    public bool Update(Room room)
+    {
+        return roomRepository.Update(room);
+    }
+
+    public bool Delete(int id)
+    {
+        return roomRepository.Delete(id);
+    }
+}
