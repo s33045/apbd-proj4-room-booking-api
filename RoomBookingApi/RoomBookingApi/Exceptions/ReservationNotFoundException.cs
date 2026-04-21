a5757206e755707892d5aadb33d0f36c660bcd60
s33045 <s33045@pjwstk.edu.pl>
2026-04-21 18:54:49 +0200

    feat(Exceptions): add ReservationNotFoundException and RoomNotFoundException

---
+++ RoomBookingApi/RoomBookingApi/Exceptions/ReservationNotFoundException.cs
@@ -0,0 +1,3 @@
+﻿namespace RoomBookingApi.Exceptions;
+
+public class ReservationNotFoundException(int id) : Exception($"Reservation with {id} not found");
---
+++ RoomBookingApi/RoomBookingApi/Exceptions/RoomNotFoundException.cs
@@ -0,0 +1,3 @@
+﻿namespace RoomBookingApi.Exceptions;
+
+public class RoomNotFoundException(int id) : Exception($"Room with {id} not found");
